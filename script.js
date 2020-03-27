185ce4a554861a59b08a0f1acfc4300e211a5631
matthew <matthew.feranmi@outlook.com>
2020-03-27 12:15:57 +0100

    first JavaScript Task

---
+++ script.js
@@ -0,0 +1,29 @@
+
+let myName = {
+    firstName: 'Matthew',
+    lastName : 'Adesina'
+};
+
+console.log(myName);
+
+let Track;
+console.log('Track = frontEnd')
+
+let courses = ['HTML', 'CSS', 'JavaScript'];
+console.log(courses);
+
+
+for (let i=0; i<200; i++)
+{
+if (i % 2 !== 0) 
+{
+    console.log(i);
+}
+}
+
+
+
+
+
+
+
